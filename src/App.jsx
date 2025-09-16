c22ec8f0b4db66021b76bc43622c271e52c191d8
eraldvelcani <eraldvelcani2003@gmail.com>
2025-09-16 17:20:04 +0200

    first commit: added products database and images, getting started of APP.jsx

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return ( 
+  <div className="min-h-screen bg-red-100 p-6">
+    <h1 className="text-3xl font-bold mb-6">Shopping Cart UI</h1>
+  </div>
+  );
+}
+ 
+export default App;
